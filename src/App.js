57da19c9dbe5bf808edff8e5e1a7a91060e28645
Vinicius do Amaral <viniciusamaralpro2@gmail.com>
2021-07-11 17:33:29 -0300

    Adicionei função de sortear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  View,
   TouchableOpacity,
-  Text,
   StyleSheet,
   StatusBar,
 } from 'react-native';
+import {useSelector} from 'react-redux';
 
 import Colors from './settings/Colors';
 import Header from './components/Header';
@@ -15,13 +14,28 @@ import TableNames from './components/TableNames';
 import IconSpin from './assets/icons/spin.svg';
 
 const App = () => {
+  const state = useSelector(state => state.users);
+  const [nameSorted, setNameSorted] = useState('');
+  const sort = () => {
+    const index = Math.floor(Math.random() * (state.length - 1 + 1)) + 1;
+
+    state.forEach(p =>
+      p.id === index
+        ? p.name != nameSorted
+          ? setNameSorted(p.name)
+          : sort()
+        : false,
+    );
+    console.warn(nameSorted);
+  };
+
   return (
     <>
       <SafeAreaView style={styles.container}>
         <Header />
         <InputName />
         <TableNames />
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={() => sort()} style={styles.button}>
           <IconSpin />
         </TouchableOpacity>
       </SafeAreaView>
